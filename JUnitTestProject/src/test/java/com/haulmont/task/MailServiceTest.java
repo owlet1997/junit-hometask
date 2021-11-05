dc9c928670c8f6ca81afc1d955365f597038d869
owlet1997 <alonewithlight@yandex.ru>
2021-11-05 15:28:11 +0400

    first commit

---
+++ JUnitTestProject/src/test/java/com/haulmont/task/MailServiceTest.java
@@ -0,0 +1,16 @@
+package com.haulmont.task;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MailServiceTest {
+
+    @Test
+    public void sendMessage() {
+    }
+
+    @Test
+    public void removeMessage() {
+    }
+}
